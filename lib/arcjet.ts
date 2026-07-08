d62f29dcb74eac30961b1f50d7c5cd0bf681fd40
Nguyen Xuan Phong <ngxphong03@gmail.com>
2026-07-08 23:36:23 +0700

    feat: add arcjet

---
+++ lib/arcjet.ts
@@ -0,0 +1,11 @@
+import { env } from '@/lib/env'
+import arcjet, { detectBot, detectPromptInjection, sensitiveInfo, shield, tokenBucket } from '@arcjet/next'
+
+export { detectBot, detectPromptInjection, sensitiveInfo, shield, tokenBucket }
+
+export default arcjet({
+    key: env.ARCJET_KEY,
+    characteristics: ['fingerprint'],
+    // define base rules here, can also be empty if you don't want to have any base rules
+    rules: [shield({ mode: 'LIVE' })],
+})
