59de0c1fb63d42e61441fbda6709f1396e867813
Nguyen Xuan Phong <ngxphong03@gmail.com>
2026-07-20 21:09:21 +0700

    [feat]: create mutation layer, protect endpoints

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,24 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {/* config options here */}
+const nextConfig: NextConfig = {
+    images: {
+        formats: ['image/webp'],
+        minimumCacheTTL: 2678400,
+        remotePatterns: [
+            { protocol: 'https', hostname: '**' },
+            { protocol: 'http', hostname: '**' },
+        ],
+        deviceSizes: [430, 768, 1080, 1280, 1600, 1920],
+        qualities: [75, 85, 95, 100], // Config quality values để tránh warning trong Next.js 16
+    },
+    reactStrictMode: false,
+    output: 'standalone',
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+}
 
 export default nextConfig
