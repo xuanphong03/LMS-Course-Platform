75dde360cd3cb77db7c2756c94ee9ce13ea70b11
Nguyen Xuan Phong <ngxphong03@gmail.com>
2026-08-01 23:00:39 +0700

    [feat]: creating Analytics Page

--- app/data/admin/require-admin.ts
+++ app/data/admin/require-admin.ts
@@ -1,22 +1,19 @@
 import 'server-only'
-
 import { ROUTES } from '@/consts/routes'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { cache } from 'react'
 
-export async function requireAdmin() {
+export const requireAdmin = cache(async () => {
     const session = await auth.api.getSession({
         headers: await headers(),
     })
-
     if (!session) {
         return redirect(ROUTES.LOGIN)
     }
-
     if (session.user.role !== 'admin') {
         return redirect(ROUTES.FORBIDDEN)
     }
-
     return session
-}
+})
--- app/data/user/require-user.ts
+++ app/data/user/require-user.ts
@@ -3,15 +3,14 @@ import { ROUTES } from '@/consts/routes'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { cache } from 'react'
 
-export async function requireUser() {
+export const requireUser = cache(async () => {
     const session = await auth.api.getSession({
         headers: await headers(),
     })
-
     if (!session) {
         return redirect(ROUTES.LOGIN)
     }
-
     return session
-}
+})
